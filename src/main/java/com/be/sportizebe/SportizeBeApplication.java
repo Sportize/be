308907311fd2e38e4f1e6c55ef91519dc43da9aa
imjuyongp <imjuyongp@gmail.com>
2026-01-15 15:48:24 +0900

    :tada:Start: Initial commit

---
+++ src/main/java/com/be/sportizebe/SportizeBeApplication.java
@@ -0,0 +1,13 @@
+package com.be.sportizebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportizeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportizeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/be/sportizebe/SportizeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.sportizebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportizeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
