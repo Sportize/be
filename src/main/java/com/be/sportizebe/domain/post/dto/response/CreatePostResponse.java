e84bcda7eaaf1895b8c61462192c5c9b7a6bfcf8
imjuyongp <imjuyongp@gmail.com>
2026-01-21 19:58:24 +0900

    :recycle:Refactor: 게시글 생성 시 응답 값에 title 추가

--- src/main/java/com/be/sportizebe/domain/post/dto/response/CreatePostResponse.java
+++ src/main/java/com/be/sportizebe/domain/post/dto/response/CreatePostResponse.java
@@ -8,14 +8,16 @@ import lombok.Builder;
 @Schema(title = "CreatePostResponse DTO", description = "게시글 생성 응답")
 public record CreatePostResponse(
     @Schema(description = "게시글 고유번호", example = "1") Long postId,
+    @Schema(description = "게시글 제목", example = "게시글 제목 ...") String title,
     @Schema(description = "게시글 내용", example = "게시글 글 내용 ...") String content,
     @Schema(description = "게시글 등록자", example = "사용자명") String publisher,
     @Schema(description = "게시글 사진 url", example = "s3 url") String imgUrl,
     @Schema(description = "익명 여부", example = "true:익명 / false:사용자명") String isAnonymous) {
 
-  public static CreatePostResponse from(Post post) { // Post -> Resonse 변환
+  public static CreatePostResponse from(Post post) { // Post -> Response 변환
     return CreatePostResponse.builder()
         .postId(post.getId())
+        .title(post.getTitle())
         .content(post.getContent())
         .publisher(post.isAnonymous() ? "익명" : "사용자명") // TODO: User 매핑 필요
         .imgUrl(post.getImgUrl())
