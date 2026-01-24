bb7a49e69ca756755865e7a0e304218698657fcb
imjuyongp <imjuyongp@gmail.com>
2026-01-24 19:11:21 +0900

    :truck:Rename: 패키지 구조 수정 + 주석 추가

--- src/main/java/com/be/sportizebe/domain/chat/websocket/handler/ChatStompController.java
+++ src/main/java/com/be/sportizebe/domain/chat/websocket/handler/ChatStompController.java
@@ -1,4 +1,4 @@
-package com.be.sportizebe.domain.chat.websocket;
+package com.be.sportizebe.domain.chat.websocket.handler;
 
 import com.be.sportizebe.domain.chat.dto.response.ChatMessageResponse;
 import com.be.sportizebe.domain.chat.dto.request.ChatSendRequest;
@@ -18,7 +18,11 @@ public class ChatStompController {
     private final ChatRoomService chatRoomService;   // ✅ 추가
     private final SimpMessagingTemplate messagingTemplate;
 
-    @MessageMapping("/chat.send")
+    /**
+     * 클라이언트가 SEND로 보낸 메세지를 받는 주소가 여기
+     * @param req -> payload
+     */
+    @MessageMapping("/chat.send") // STOMP -> SEND (@MessageMapping : SEND frame)
     public void send(ChatSendRequest req){
 
         ChatRoom room = chatRoomService.getOrThrow(req.getRoomId());
@@ -28,9 +32,15 @@ public class ChatStompController {
                 req.getSenderNickname(),
                 req.getContent()
         );
+
+        /**
+         * 채팅방을 구독 중인 모든 클라이언트에게 메시지를 뿌리는 로직
+         * messagingTemplate : 서버 → 클라이언트 메시지 전송용 객체, STOMP 브로커에게 메세지를 던지는 역할
+         * 객체 → 메시지로 바꿔서 → 목적지로 보낸다
+         */
         messagingTemplate.convertAndSend(
-                "/sub/chat/rooms/" + req.getRoomId(),
-                ChatMessageResponse.from(saved)
+            "/sub/chat/rooms/" + req.getRoomId(), // 채팅방 단위 브로드캐스트
+            ChatMessageResponse.from(saved)
         );
     }
 }
