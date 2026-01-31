52e3fafd3613a269057871a12c73b3713d5433fd
imjuyongp <imjuyongp@gmail.com>
2026-02-01 00:44:55 +0900

    :recycle:Refactor: @AuthenticationPrincipal 사용하여 보안 강화

--- src/main/java/com/be/sportizebe/domain/user/controller/UserController.java
+++ src/main/java/com/be/sportizebe/domain/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.be.sportizebe.domain.user.controller;
 import com.be.sportizebe.domain.user.dto.request.SignUpRequest;
 import com.be.sportizebe.domain.user.dto.response.ProfileImageResponse;
 import com.be.sportizebe.domain.user.dto.response.SignUpResponse;
+import com.be.sportizebe.domain.user.entity.User;
 import com.be.sportizebe.domain.user.service.UserServiceImpl;
 import com.be.sportizebe.global.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -31,13 +33,13 @@ public class UserController {
             .body(BaseResponse.success("회원가입 성공", response));
     }
 
-    @PostMapping(value = "/{userId}/profile-image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/profile-image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "프로필 사진 업로드", description = "사용자 프로필 사진을 업로드합니다. (최대 5MB, jpg/jpeg/png/gif/webp 지원)")
     public ResponseEntity<BaseResponse<ProfileImageResponse>> uploadProfileImage(
-        @PathVariable Long userId,
+        @AuthenticationPrincipal User user,
         @RequestPart("file") MultipartFile file
     ) {
-        ProfileImageResponse response = userService.uploadProfileImage(userId, file);
+        ProfileImageResponse response = userService.uploadProfileImage(user.getId(), file);
         return ResponseEntity.ok(BaseResponse.success("프로필 사진 업로드 성공", response));
     }
 }
