0b69dcc60754dc29e4abca9f90befecb0c93903e
imjuyongp <imjuyongp@gmail.com>
2026-02-01 01:07:35 +0900

    :bug:Fix: aws 자격증명 설정 StaticCredentialsProvider으로 변경

--- src/main/java/com/be/sportizebe/global/config/S3Config.java
+++ src/main/java/com/be/sportizebe/global/config/S3Config.java
@@ -4,7 +4,8 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -21,11 +22,21 @@ public class S3Config {
   @Value("${spring.cloud.aws.s3.path.club}")
   private String club;
 
+  @Value("${spring.cloud.aws.credentials.access-key}")
+  private String accessKey;
+
+  @Value("${spring.cloud.aws.credentials.secret-key}")
+  private String secretKey;
+
   @Bean
   S3Client s3Client() {
+    AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
     return S3Client.builder()
       .region(Region.AP_NORTHEAST_2)
-      .credentialsProvider(DefaultCredentialsProvider.create())
+      // DefaultCredentialsProvider: 환경 변수나 AWS 프로필 파일에서 자격 증명을 찾음
+      // StaticCredentialsProvider: 개발환경에서는 application.yml에서 찾도록 설정
+      .credentialsProvider(StaticCredentialsProvider.create(credentials))
       .build();
   }
 }
