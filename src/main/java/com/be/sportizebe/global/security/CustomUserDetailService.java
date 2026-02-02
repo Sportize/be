46445b10c206b0ba0960a484ea3b933784cc00e8
안훈기 <angora@anhungiui-MacBookAir.local>
2026-02-03 02:20:13 +0900

    :sparkles:Feat: UserDetails 로딩 캐시 적용

--- src/main/java/com/be/sportizebe/global/security/CustomUserDetailService.java
+++ src/main/java/com/be/sportizebe/global/security/CustomUserDetailService.java
@@ -3,6 +3,7 @@ package com.be.sportizebe.global.security;
 import com.be.sportizebe.domain.user.entity.User;
 import com.be.sportizebe.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,6 +14,8 @@ import org.springframework.stereotype.Service;
 public class CustomUserDetailService implements UserDetailsService {
   private final UserRepository userRepository;
 
+  @Cacheable(cacheNames = "userDetails", key = "#username", unless = "#result == null")
+  // result == null: 유저가 있으면- > 캐시에 저장, 유저가 없으면 -> 캐시에 저장 X
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     User user = userRepository.findByUsername(username)
